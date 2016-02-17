7671b3a944caa484822a49618f64923df3748949
coverty1dk <coverty1dk@gmail.com>
2016-02-17 20:57:11 +0100

    Opprettet ServerController, koblet sammen med server.mockup.fxml
    SendButton kodet opp med ActionEvent

---
+++ src/main/java/ServerController.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Erlend on 10.03.2015.
+ */
+
+package main.java;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+
+public class ServerController
+{
+    @FXML
+    private void sendButtonAction(ActionEvent event)
+    {
+        System.out.println("Send kommando");
+    }
+}
