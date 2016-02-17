1f2f2315357e3b7cf586b40ac00b9d551fd7b926
coverty1dk <coverty1dk@gmail.com>
2016-02-17 11:57:55 +0100

    serverapp

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -12,27 +12,22 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class Server extends Application
-{
-    public static void main(String ... args)
+public class Server extends Application {
+    public static void main(String... args)
     {
         launch(args);
     }
 
     @Override
     public void start(Stage server) throws IOException {
-
-
         Parent parent = FXMLLoader.load(getClass().getResource("gui/server.mockup.fxml"));
         Scene scene = new Scene(parent);
         server.setTitle("Server");
         server.setScene(scene);
         server.show();
-
     }
 
-    public void send()
-    {
-        System.out.println("Kommando send klient");
-    }
+
 }
+
+
