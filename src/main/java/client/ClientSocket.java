8da887c87d3effb095416d231ba2d9f41d1bc78d
Simon <simon.bjonsson@gmail.com>
2016-02-28 16:18:05 +0100

    Client thread closing on exit/close

--- src/main/java/client/ClientSocket.java
+++ src/main/java/client/ClientSocket.java
@@ -2,12 +2,12 @@ package main.java.client;
 
 import main.java.view.Logger;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+
 
 /**
  * Created by Simon on 17.02.2016.
@@ -20,9 +20,8 @@ public class ClientSocket extends Thread
     private static final int THREAD_SLEEP = 100;
     private static final String HARTBEAT = "tock";
     private static final String SERVER_BEAT = "tick";
-
     private static ClientController controller;
-
+    private boolean running = true;
     private Logger logger;
 
 
@@ -31,46 +30,59 @@ public class ClientSocket extends Thread
         this.controller=controller;
     }
 
+    public void kill()
+    {
+
+        running=false;
+    }
+
 
     @Override
     public void run() {
         super.run();
 
-        try (
-                Socket soc = new Socket(host, port);
-                PrintWriter out =
-                        new PrintWriter(soc.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(soc.getInputStream()));
-        )
-        {
-            Protocolparser parser =  new Protocolparser(controller);
-            String receivedText;
-            while ((receivedText = in.readLine()) != null) {
 
-                Thread.sleep(THREAD_SLEEP);
+            try (
+                    Socket soc = new Socket(host, port);
+                    PrintWriter out =
+                            new PrintWriter(soc.getOutputStream(), true);
 
-                if (!receivedText.equals(SERVER_BEAT)) {
-                   if(parser.signalparse(receivedText)) {
-                       controller.setLights(parser.getPayload());
-                       pushToLogger(parser.getPayload().toString());
-                   }
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(soc.getInputStream()))
+            )
+            {
+                Protocolparser parser =  new Protocolparser(controller);
+                String receivedText;
+                while ((receivedText = in.readLine()) != null) {
+
+                    Thread.sleep(THREAD_SLEEP);
+                    if(!running){
+                        throw new InterruptedException();
+                    }
+                    if (!receivedText.equals(SERVER_BEAT)) {
+                        if(parser.signalparse(receivedText)) {
+                            controller.setLights(parser.getPayload());
+                            pushToLogger(parser.getPayload().toString());
+                        }
+                    }
+                    out.println(HARTBEAT);
+
                 }
-                out.println(HARTBEAT);
+
+                System.out.println("Server disconnected");
+
+            } catch (UnknownHostException uhe) {
+                System.err.printf("Unknown host %s", host);
+                System.exit(1);
+            } catch (IOException ioe) {
+                System.err.printf("Could not get IO from the connection to: %s", host);
+                System.exit(1);
+            }catch(InterruptedException ie)
+            {
+                System.exit(1);
             }
 
-            System.out.println("Server disconnected");
 
-        } catch (UnknownHostException uhe) {
-            System.err.printf("Unknown host %s", host);
-            System.exit(1);
-        } catch (IOException ioe) {
-            System.err.printf("Could not get IO from the connection to: %s", host);
-            System.exit(1);
-        }catch(InterruptedException ie)
-        {
-
-        }
     }
 
     private void pushToLogger(String str)
